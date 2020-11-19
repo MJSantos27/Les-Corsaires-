0a8a51374e1f4aaaa9cf4577af8c7fc1e5290657
Miguel Santos <migueljsantos27@gmail.com>
2020-11-19 18:21:08 +0100

    add some css to header comp

--- les-pirates/src/Components/Header.jsx
+++ les-pirates/src/Components/Header.jsx
@@ -1,10 +1,32 @@
 import React from "react";
-
-function Navbar() {
+import "./Header.css";
+function Header() {
   return (
-    <div className="Navbar">
-      <h1>Les Corsaire</h1>
-    </div>
+    <nav>
+      <div className="title">
+        <h1>Les Corsaire</h1>
+      </div>
+      <img
+        className="logo"
+        src="https://tomato.to/tmp/5fb695876c73c.jpg"
+        alt="Pirate symbol"
+      />
+      <ul className="navLinks">
+        <li>
+          <a>Home</a>
+        </li>
+        <li>
+          <a>Product</a>
+        </li>
+        <li>
+          <a>Services</a>
+        </li>
+        <li>
+          <a>AboutUs</a>
+        </li>
+      </ul>
+      <i class="fas fa-bars"></i>
+    </nav>
   );
 }
 export default Header;
