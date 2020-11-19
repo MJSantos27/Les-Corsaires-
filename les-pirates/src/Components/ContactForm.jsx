e8edccbe2bd52ea935f2e3532cba5945a0729507
CASTAN <christophecastan4@gmail.com>
2020-11-19 17:18:57 +0100

    add div

--- les-pirates/src/Components/ContactForm.jsx
+++ les-pirates/src/Components/ContactForm.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class ContactForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: "coconut" };
+    this.state = { value: "Anne Bonny" };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -13,37 +13,45 @@ class ContactForm extends React.Component {
   }
 
   handleSubmit(event) {
-    alert("Je postule pour l équipe de : " + this.state.value);
+    alert("I am applying for the team of : " + this.state.value);
     event.preventDefault();
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label>
-          Choisissez votre équipe :
-          <select value={this.state.value} onChange={this.handleChange}>
-            <option value="grapefruit">Pamplemousse</option>
-            <option value="lime">Citron vert</option>
-            <option value="coconut">Noix de coco</option>
-            <option value="mango">Mangue</option>
-          </select>
-        </label>
-        <input type="submit" value="Send" />
-        <div className="pirate identity">
+        <h2>I am applying for the team of :</h2>
+        <div clasName="selector">
+          <label>
+            Choose your team :
+            <select value={this.state.value} onChange={this.handleChange}>
+              <option value="Anne Bonny">Anne Bonny</option>
+              <option value="Bartholomew Roberts">Bartholomew Roberts</option>
+              <option value="Benjamin Hornigold">Benjamin Hornigold</option>
+              <option value="Calico Jack">Calico Jack</option>
+            </select>
+          </label>
+          <input type="submit" value="Send" />
+        </div>
+        <div className="firstName">
           <label>
             first name:
             <input type="text" />
           </label>
-          <br />
+        </div>
+        <div className="lastName">
           <label>
             last name:
             <input type="text" />
           </label>
+        </div>
+        <div className="questionRealPirate">
           <label>
-            your message:
+            Are you a real pirate ?
             <textarea />
           </label>
+        </div>
+        <div className="email">
           <label>
             email:
             <input type="text" placeholder="piratedescaraïbes@gmail.com" />
