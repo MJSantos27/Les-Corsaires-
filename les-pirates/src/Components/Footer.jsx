add0e8f402197187efa2e79d9229293c5999e79a
CASTAN <christophecastan4@gmail.com>
2020-11-20 10:36:11 +0100

    add className

--- les-pirates/src/Components/Footer.jsx
+++ les-pirates/src/Components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Footer.css";
 
 function Footer() {
   return (
@@ -7,14 +8,17 @@ function Footer() {
         <img
           src="https://www.cmonsite.fr/creer-un-site/wp-content/uploads/2013/03/facebook-icone-CmonSite.png"
           alt="Facebook-icone"
+          className="icone"
         />
         <img
           src="https://www.cmonsite.fr/creer-un-site/wp-content/uploads/2013/03/twitter-CmonSite.png"
           alt="Twitter-icone"
+          className="icone"
         />
         <img
           src="https://www.cmonsite.fr/creer-un-site/wp-content/uploads/2019/01/logo-instagram-300x300.png"
           alt="Instagram-icone"
+          className="icone"
         />
         <p>Site & content created by The Corsairs</p>
         <p>Copyright © 1756-1757</p>
