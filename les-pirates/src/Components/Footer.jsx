38311a744845e041f28e621cad1a93c24b65d4ff
Miguel Santos <migueljsantos27@gmail.com>
2020-11-20 09:42:36 +0100

    resolved conflits

--- les-pirates/src/Components/Footer.jsx
+++ les-pirates/src/Components/Footer.jsx
@@ -1,12 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 
-function Footer(props) {
+function Footer() {
   return (
     <footer>
       <div className="myFooter">
-        <img src="https://www.cmonsite.fr/creer-un-site/wp-content/uploads/2013/03/facebook-icone-CmonSite.png"></img>
-        <img src="https://www.cmonsite.fr/creer-un-site/wp-content/uploads/2013/03/twitter-CmonSite.png"></img>
-        <img src="https://www.cmonsite.fr/creer-un-site/wp-content/uploads/2019/01/logo-instagram-300x300.png"></img>
+        <img
+          src="https://www.cmonsite.fr/creer-un-site/wp-content/uploads/2013/03/facebook-icone-CmonSite.png"
+          alt="Facebook-icone"
+        />
+        <img
+          src="https://www.cmonsite.fr/creer-un-site/wp-content/uploads/2013/03/twitter-CmonSite.png"
+          alt="Twitter-icone"
+        />
+        <img
+          src="https://www.cmonsite.fr/creer-un-site/wp-content/uploads/2019/01/logo-instagram-300x300.png"
+          alt="Instagram-icone"
+        />
         <p>Site & content created by The Corsairs</p>
         <p>Copyright © 1756-1757</p>
         <p>All rights reserved</p>
